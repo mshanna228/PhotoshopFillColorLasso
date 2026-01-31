d405dc46f24ef2e5c4f2463338c3994182a181cb
mshanna228 <mrshanna613@gmail.com>
2026-02-01 00:09:54 +0300

    Update LassoAutoFillAction2.jsx

--- PluginLasso/LassoAutoFillAction2.jsx
+++ PluginLasso/LassoAutoFillAction2.jsx
@@ -17,19 +17,4 @@ try {
 } catch (err) {
 }
 
-// // Этот код заставляет лассо красить САМО сразу после выделения
-// try {
-//     if (app.documents.length > 0) {
-//         var doc = app.activeDocument;
-//         var hasSelection = false;
-//         try { if (doc.selection.bounds) hasSelection = true; } catch (e) {}
-
-//         if (hasSelection) {
-//             var newLayer = doc.artLayers.add();
-//             newLayer.name = "Мазок " + (doc.artLayers.length);
-            
-//             doc.selection.fill(app.foregroundColor);
-//             doc.selection.deselect();
-//         }
-//     }
-// } catch (err) {}
+// Этот код заставляет лассо красить САМО сразу после выделения
