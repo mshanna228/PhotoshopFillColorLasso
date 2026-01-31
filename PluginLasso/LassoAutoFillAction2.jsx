32e28de9df34b3687abc1c725f7d686db8a241e7
mshanna228 <mrshanna613@gmail.com>
2026-02-01 00:08:53 +0300

    Update LassoAutoFillAction2.jsx

--- PluginLasso/LassoAutoFillAction2.jsx
+++ PluginLasso/LassoAutoFillAction2.jsx
@@ -2,24 +2,19 @@ try {
     if (app.documents.length > 0) {
         var doc = app.activeDocument;
         
-        // Проверяем, есть ли выделение
         var hasSelection = false;
         try { if (doc.selection.bounds) hasSelection = true; } catch (e) {}
 
         if (hasSelection) {
-            // Создаем новый слой над текущим
             var newLayer = doc.artLayers.add();
             newLayer.name = "Lasso Fill " + (doc.artLayers.length);
             
-            // Заливка цветом, который выбран основным на панели
             doc.selection.fill(app.foregroundColor);
             
-            // Снимаем выделение, чтобы сразу рисовать дальше
             doc.selection.deselect();
         }
     }
 } catch (err) {
-    // Ошибки не мешают рисованию
 }
 
 // // Этот код заставляет лассо красить САМО сразу после выделения
@@ -30,7 +25,6 @@ try {
 //         try { if (doc.selection.bounds) hasSelection = true; } catch (e) {}
 
 //         if (hasSelection) {
-//             // Создаем новый слой, чтобы мазки были отдельными
 //             var newLayer = doc.artLayers.add();
 //             newLayer.name = "Мазок " + (doc.artLayers.length);
             
