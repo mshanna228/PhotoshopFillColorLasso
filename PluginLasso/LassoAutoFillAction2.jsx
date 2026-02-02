e280238c4a5757ee0595a525341410d427912c5a
mshanna228 <mrshanna613@gmail.com>
2026-02-02 23:55:02 +0300

    Clean up comments in LassoAutoFillAction2.jsx
    Removed comments in Russian to clean up the code.

--- PluginLasso/LassoAutoFillAction2.jsx
+++ PluginLasso/LassoAutoFillAction2.jsx
@@ -2,20 +2,16 @@ try {
     if (app.documents.length > 0) {
         var doc = app.activeDocument;
         
-        // Проверяем, есть ли выделение
         var hasSelection = false;
         try { if (doc.selection.bounds) hasSelection = true; } catch (e) {}
 
         if (hasSelection) {
-            // Заливка на ТЕКУЩЕМ слое основным цветом
             doc.selection.fill(app.foregroundColor);
             
-            // Снимаем выделение, чтобы можно было сразу рисовать дальше
             doc.selection.deselect();
         }
     }
 } catch (err) {
-    // Ошибки игнорируем, чтобы не прерывать процесс рисования
 }
 
 // try {
